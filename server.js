958b7316e11de30eed5d5f33300039ab3c45560a
Stian Bakkane <stianba@gmail.com>
2016-01-08 10:37:24 +0100

    Configured mongolab server connection

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use('/', routes);
 
 // Connect to mongodb server
 mongoose.connect('mongodb://'
-  //+ process.env.MONGO_USER + ':'
-  //+ process.env.MONGO_PASSWORD + '@'
+  + process.env.MONGO_USER + ':'
+  + process.env.MONGO_PASSWORD + '@'
   + process.env.MONGO_HOST + '/'
   + process.env.MONGO_DB
 );
