4579a057b3ae8eb4037783a37960ac1452290709
Stian Bakkane <stianba@gmail.com>
2016-01-08 11:37:24 +0100

    Added post and get routes for services

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {adminAuth} from './auth';
+import Services from './controllers/services';
 import Franchises from './controllers/franchises';
 
 const router = express.Router();
@@ -7,6 +8,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.json({msg: `Hello world.`});
 });
+
+// Service routes
+router.get('/services', Services.index);
+router.post('/services', adminAuth, Services.create);
 
 // Franchise routes
 router.get('/franchises', Franchises.index);
